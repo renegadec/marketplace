77f41c90a2420e65a431c16c552c7caf7b1a7000
Isheanesu <inmisimail@gmail.com>
2022-12-31 11:13:17 +0200

    Added social logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,27 @@
 import logo from "./logo.png";
+import whitelogo from "./white_logo.png"
 import heroImg from "./hero.png"
 import Avocados from "./avocados.png";
 import Apples from "./apples.png";
 import Like from "./like.png";
-import Bananas from "./bananas.png"
+import Bananas from "./bananas.png";
+import Facebook from "./social/facebook.svg";
+import Github from "./social/github.svg";
+import Instagram from "./social/instagram.svg";
+import Linkedin from "./social/linkedin.svg";
+import Mail from "./social/mail.svg";
 
-export { logo, heroImg, Avocados, Like, Apples, Bananas };
+export { 
+    logo, 
+    whitelogo,
+    heroImg, 
+    Avocados, 
+    Like, 
+    Apples, 
+    Bananas,
+    Facebook,
+    Github,
+    Instagram,
+    Linkedin,
+    Mail 
+};
