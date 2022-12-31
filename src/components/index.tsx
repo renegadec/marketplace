5cbb19e7d7c675403b045ba6c7cdb1576eda395b
Isheanesu <inmisimail@gmail.com>
2022-12-31 14:39:21 +0200

    Added button export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,15 +1,19 @@
+import Button from "./Button/Button";
 import Features from "./Features";
 import Hero from "./Hero";
 import Info from "./Info";
 import Navbar from "./Navbar";
 import Product from "./Product";
 import ProductCard from "./ProductCard/ProductCard";
+import Footer from "./Footer/Footer";
 
 export {
+    Button,
     Features,
     Hero,
     Info,
     Navbar,
     Product,
-    ProductCard
+    ProductCard,
+    Footer
 }
