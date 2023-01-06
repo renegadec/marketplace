c2afa5ca4483dfe632fc418f1a4b8bdc9d95f7e3
Isheanesu <inmisimail@gmail.com>
2023-01-06 08:13:20 +0200

    Fixed image import links

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
 import {  Avocados, Apples, Bananas } from "../assets";
 
-
 export const navigation = [
   { name: "Product", href: "#" },
   { name: "Features", href: "#" },
@@ -14,19 +13,19 @@ export const productsData = {
       id: 1,
       type: "Avocados",
       desc: "Southern Africa green-skin variety.",
-      image: "./src/assets/avocados.png",
+      image: "../src/assets/avocados.png",
     },
     {
       id: 2,
       type: "Apples",
       desc: "Sweet apples from eastern Zimbabwe.",
-      image: "./src/assets/apples.png",
+      image: "../src/assets/apples.png",
     },
     {
       id: 3,
       type: "Bananas",
       desc: "Sweet bananas from eastern Zimbabwe.",
-      image: "./src/assets/bananas.png",
+      image: "../src/assets/bananas.png",
     },
   ],
 };
