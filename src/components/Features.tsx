5912ddd41a8bf322b0875118e49f9b87435fd622
africoiner <confidencenyirenda7@gmail.com>
2023-01-12 21:52:46 +0200

    edited featured products and added mobile responsiveness

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -4,23 +4,25 @@ import { productsData } from '../constants'
 
 const Features = (props) => {
     return (
+        <section className="">
+            <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h2 className="text-2xl mb-16 font-bold tracking-tight text-primary">Best Selling Products</h2>
 
-        <section id='featured' className='m-[70px] flex flex-col justify-center'>
-            <div className='flex justify-center items-center flex-col pb-7'>
-                <h1 className='text-3xl font-bold text-primary'>Featured Products</h1>
-                <hr className='w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700' />
-                <h3 className='text-2xl text-black'>These are the top performing products.</h3>
-            </div>
-            <div className="px-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-12">
-                {productsData['product'].slice(0, 3).map((product) => (
-                    <ProductCard 
-                        key={product.id}
-                        id={String(product.id - 1)}
-                        type={product.type}
-                        desc={product.desc}
-                        image={product.image}
-                    />
-                ))}
+                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                    {productsData['product'].slice(0, 3).map((product) => (
+                        <div className='group relative'>
+                            <div className='flex justify-center w-full lg:aspect-none lg:h-80'>
+                                <ProductCard 
+                                    key={product.id}
+                                    id={String(product.id - 1)}
+                                    type={product.type}
+                                    desc={product.desc}
+                                    image={product.image}
+                                />
+                            </div>
+                        </div>       
+                        ))}
+                </div>
             </div>
         </section>
     )
