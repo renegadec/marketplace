87ac528b9e26b70b30fe290df9a85150d46ea438
Isheanesu <inmisimail@gmail.com>
2023-01-17 20:03:40 +0200

    Added login

--- src/components/Authentication/Login.tsx
+++ src/components/Authentication/Login.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { logo } from '../../assets'
+import { supabase } from '../../config/supabase'
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
+
+    const LoginHandler = async (e) => {  
+        
+        e.preventDefault()
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        }) 
+
+        if(data.user) {
+            navigate("/market")
+        }
+
+        if(error) {
+            console.log(error)
+        }
+
+    }
     return (
         <section className="">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -16,11 +40,11 @@ const Login = () => {
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-primary">Your email</label>
-                                <input type="email" name="email" id="email" className=" border border-primary text-black sm:text-sm rounded-lg focus:ring-primary focus:border-primary-600 block w-full p-2.5 dark:border-primary dark:placeholder-gray-400 dark:text-black  dark:focus:border-primary" placeholder="name@company.com" required />
+                                <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} className=" border border-primary text-black sm:text-sm rounded-lg focus:ring-primary focus:border-primary-600 block w-full p-2.5 dark:border-primary dark:placeholder-gray-400 dark:text-black  dark:focus:border-primary" placeholder="name@company.com" required />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-primary">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="border border-primary text-black sm:text-sm rounded-lg focus:ring-primary focus:border-primary-600 block w-full p-2.5  dark:border-primary dark:placeholder-gray-400 dark:text-black  dark:focus:border-primary" required />
+                                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} id="password" placeholder="••••••••" className="border border-primary text-black sm:text-sm rounded-lg focus:ring-primary focus:border-primary-600 block w-full p-2.5  dark:border-primary dark:placeholder-gray-400 dark:text-black  dark:focus:border-primary" required />
                             </div>
                             <div className="flex items-center justify-between">
                                 <div className="flex items-start">
@@ -33,7 +57,7 @@ const Login = () => {
                                 </div>
                                 <a href="#" className="text-sm font-medium text-primary hover:underline dark:text-primary">Forgot password?</a>
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 ring-primary focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary-700 dark:focus:ring-primary">Sign in</button>
+                            <button type="submit" onClick={(e) => LoginHandler(e)} className="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 ring-primary focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary-700 dark:focus:ring-primary">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <a href="#" className="font-medium text-primary hover:underline dark:text-primary">Sign up</a>
                             </p>
