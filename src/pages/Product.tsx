fea1e2e6aed8f5d417f47f3a1c4bd37ff04bd455
Isheanesu <inmisimail@gmail.com>
2023-01-30 17:42:47 +0200

    Changed section links

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -70,115 +70,115 @@ const Product = () => {
 
     return (
         <div className="px-7 lg:px-28 pt-8 pb-10">     
-            <section id="top"> 
-            <div className="grid grid-rows-1 md:flex flex-row mb-12">
-                    <div className="hidden md:w-2/12 justify-center md:flex flex-col md:justify-start md:items-start">
-                        {productsData['product'][id]['images'].map((imageSrc, index) => (
-                            <button onClick={() => setActiveImg(index)}
-                                key={index}
-                                className="h-24 w-24 my-2 rounded-[8px] border-primary hover:border-gray-400 border-2">
-                                <img className="h-full w-full"
-                                    src={`${imageSrc}`} 
-                                    alt="Product Image" />
-                            </button>
-                        ))}
-                    </div>
-                    <div className="flex justify-center md:px-8 md:py-2 md:w-5/12">
-                        <img 
-                            className="h-[20rem] lg:h-[27rem] w-auto border-primary border-2 rounded-[8px]"
-                            src={`${productsData['product'][id]['images'][activeImg]}`} 
-                            alt="Product Image" 
-                        />
-                    </div>
-                <div className="flex flex-col py-2 md:w-5/12">
-                    <h1 className="font-extrabold text-3xl">
-                        {productsData['product'][id]['type']}
-                    </h1>
-                    <div className="flex flex-row mt-4">
-                        <h3 className="font-semibold md:text-xl mr-6">
-                            Price/KG
-                        </h3>
-                        <h3 className="font-medium md:text-xl text-[#A18A68]">
-                            $ 15,00
-                        </h3>
-                    </div>
-                    <div className="flex flex-row mt-12">
-                        <div className="flex flex-row mr-4">
-                            {[0,1,2,3,4].map((item, index) => (
-                                <button className="mx-[1px]" key={index}>
-                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M17.9528 6.90406C17.8343 6.53958 17.511 6.28153 17.1301 6.24706L11.934 5.77533L9.88047 0.967631C9.72886 0.614414 9.38388 0.386444 8.9999 0.386444C8.61592 0.386444 8.27081 0.614414 8.12016 0.967631L6.06664 5.77533L0.869746 6.24706C0.488789 6.28222 0.166197 6.54026 0.0469935 6.90406C-0.0715235 7.26853 0.0379296 7.6683 0.326051 7.92099L4.25387 11.3651L3.09575 16.4659C3.01102 16.8409 3.15659 17.2288 3.46778 17.4537C3.63505 17.5753 3.83157 17.636 4.02878 17.636C4.19825 17.636 4.36785 17.5909 4.51933 17.5003L8.9999 14.8212L13.4797 17.5003C13.8083 17.6967 14.2215 17.6787 14.532 17.4537C14.8432 17.2288 14.9888 16.8409 14.9041 16.4659L13.7459 11.3651L17.6738 7.92099C17.9617 7.6683 18.0713 7.26936 17.9528 6.90406Z" fill="black"/>
+            <section id={`top-${id}`}> 
+                <div className="grid grid-rows-1 md:flex flex-row mb-12">
+                        <div className="hidden md:w-2/12 justify-center md:flex flex-col md:justify-start md:items-start">
+                            {productsData['product'][id]['images'].map((imageSrc, index) => (
+                                <button onClick={() => setActiveImg(index)}
+                                    key={index}
+                                    className="h-24 w-24 my-2 rounded-[8px] border-primary hover:border-gray-400 border-2">
+                                    <img className="h-full w-full"
+                                        src={`${imageSrc}`} 
+                                        alt="Product Image" />
+                                </button>
+                            ))}
+                        </div>
+                        <div className="flex justify-center md:px-8 md:py-2 md:w-5/12">
+                            <img 
+                                className="h-[20rem] lg:h-[27rem] w-auto border-primary border-2 rounded-[8px]"
+                                src={`${productsData['product'][id]['images'][activeImg]}`} 
+                                alt="Product Image" 
+                            />
+                        </div>
+                    <div className="flex flex-col py-2 md:w-5/12">
+                        <h1 className="font-extrabold text-3xl">
+                            {productsData['product'][id]['type']}
+                        </h1>
+                        <div className="flex flex-row mt-4">
+                            <h3 className="font-semibold md:text-xl mr-6">
+                                Price/KG
+                            </h3>
+                            <h3 className="font-medium md:text-xl text-[#A18A68]">
+                                $ 15,00
+                            </h3>
+                        </div>
+                        <div className="flex flex-row mt-12">
+                            <div className="flex flex-row mr-4">
+                                {[0,1,2,3,4].map((item, index) => (
+                                    <button className="mx-[1px]" key={index}>
+                                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M17.9528 6.90406C17.8343 6.53958 17.511 6.28153 17.1301 6.24706L11.934 5.77533L9.88047 0.967631C9.72886 0.614414 9.38388 0.386444 8.9999 0.386444C8.61592 0.386444 8.27081 0.614414 8.12016 0.967631L6.06664 5.77533L0.869746 6.24706C0.488789 6.28222 0.166197 6.54026 0.0469935 6.90406C-0.0715235 7.26853 0.0379296 7.6683 0.326051 7.92099L4.25387 11.3651L3.09575 16.4659C3.01102 16.8409 3.15659 17.2288 3.46778 17.4537C3.63505 17.5753 3.83157 17.636 4.02878 17.636C4.19825 17.636 4.36785 17.5909 4.51933 17.5003L8.9999 14.8212L13.4797 17.5003C13.8083 17.6967 14.2215 17.6787 14.532 17.4537C14.8432 17.2288 14.9888 16.8409 14.9041 16.4659L13.7459 11.3651L17.6738 7.92099C17.9617 7.6683 18.0713 7.26936 17.9528 6.90406Z" fill="black"/>
+                                        </svg>
+                                    </button>))}
+                            </div>
+                            <h3 className="text-gray-600 text-lg">
+                                1 customer review
+                            </h3>
+                        </div>
+                        <div>
+                            <p className="font-semibold text-xl text-gray-600 mt-2">
+                                {productsData['product'][id]['desc']}
+                            </p>
+                        </div>
+                        <div className="flex flex-row items-center mt-6">
+                            <div className="flex flex-row bg-gray-200 py-1 px-4">
+                                <button onClick={() => changeQty(false)}>
+                                    -
+                                </button>
+                                <input 
+                                    className="flex text-center focus:outline-none bg-transparent h-8 w-8 mx-2"
+                                    type="number" 
+                                    defaultValue={qty} />
+                                <button onClick={() => changeQty(true)}>
+                                    +
+                                </button>
+                            </div>
+                            <p className="font-semibold text-lg mx-4">
+                                Kgs
+                            </p>
+                            <div className="w-48">
+                                <Button 
+                                    text="Place Order" 
+                                    width="full"
+                                    round="none"
+                                    outline="primary"
+                                    variant="secondary"
+                                    handler={() => console.log()}/>
+                            </div>
+                        </div>
+                        <div className="flex flex-row mt-10">
+                            <div className="flex border-r-2 border-gray-200 pr-6">
+                                <button className="group">
+                                    <svg className="fill-primary h-4 w-5" viewBox="0 0 20 18" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
+                                        <path clipRule="evenodd" d="M9.88013 2.01405C11.0249 0.77861 12.6287 0.0520888 14.3262 0C17.3273 0 19.7603 2.39588 19.7603 5.35135C19.7603 8.85038 15.8104 12.6852 13.4415 14.9851C13.2282 15.1921 13.0278 15.3868 12.8442 15.5676L10.5915 17.7859C10.4527 17.9228 10.2643 17.9998 10.0678 18H9.6924C9.49594 17.9998 9.30759 17.9228 9.16876 17.7859L6.91609 15.5676C6.73248 15.3868 6.53202 15.1921 6.31874 14.9851C3.94983 12.6852 0 8.85038 0 5.35135C0 2.39588 2.43292 0 5.43407 0C7.13157 0.0520888 8.73538 0.77861 9.88013 2.01405ZM9.88027 15.7328L12.0539 13.6215C14.0596 11.6756 17.7844 8.03664 17.7844 5.35123C17.7844 4.44299 17.416 3.57235 16.7611 2.93287C16.1061 2.29339 15.2189 1.93805 14.2967 1.94583C12.7606 2.08626 11.3741 2.91055 10.5324 4.18367C10.4381 4.30499 10.2923 4.37677 10.1372 4.37826H9.69255C9.46171 4.37765 9.24364 4.27385 9.09974 4.0961C8.27139 2.8699 6.92358 2.07925 5.43421 1.94583C3.52439 1.94583 1.97617 3.47048 1.97617 5.35123C1.97617 8.03664 5.70098 11.6756 7.70664 13.6215L9.88027 15.7328Z" fill="#707070"/>
                                     </svg>
-                                </button>))}
+                                </button>
+                            </div>
+                            <div className="flex ml-2">
+                                <img className="h-4 w-5 ml-2" 
+                                    src={Mail} 
+                                    alt="Mail" />                            
+                                <img className="h-4 w-5 ml-2" 
+                                    src={Facebook} 
+                                    alt="Facebook" />
+                                <img className="h-4 w-5 ml-2" 
+                                    src={Instagram} 
+                                    alt="Instagram" />                                
+                            </div>
                         </div>
-                        <h3 className="text-gray-600 text-lg">
-                            1 customer review
-                        </h3>
-                    </div>
-                    <div>
-                        <p className="font-semibold text-xl text-gray-600 mt-2">
-                            {productsData['product'][id]['desc']}
-                        </p>
-                    </div>
-                    <div className="flex flex-row items-center mt-6">
-                        <div className="flex flex-row bg-gray-200 py-1 px-4">
-                            <button onClick={() => changeQty(false)}>
-                                -
-                            </button>
-                            <input 
-                                className="flex text-center focus:outline-none bg-transparent h-8 w-8 mx-2"
-                                type="number" 
-                                defaultValue={qty} />
-                            <button onClick={() => changeQty(true)}>
-                                +
-                            </button>
-                        </div>
-                        <p className="font-semibold text-lg mx-4">
-                            Kgs
-                        </p>
-                        <div className="w-48">
-                            <Button 
-                                text="Place Order" 
-                                width="full"
-                                round="none"
-                                outline="primary"
-                                variant="secondary"
-                                handler={() => console.log()}/>
+                        <div className="flex flex-row mt-4">
+                            <div className="flex flex-col mr-6">
+                                <p className="font-semibold text-primary text-sm">Min Order:</p>
+                                <p className="font-semibold text-primary text-sm">Categories:</p>
+                            </div>
+                            <div className="flex flex-col">
+                                <p className="font-semibold text-sm text-gray-600">{minOrder}</p>
+                                <p className="font-semibold text-sm text-gray-600">Fruits</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex flex-row mt-10">
-                        <div className="flex border-r-2 border-gray-200 pr-6">
-                            <button className="group">
-                                <svg className="fill-primary h-4 w-5" viewBox="0 0 20 18" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
-                                    <path clipRule="evenodd" d="M9.88013 2.01405C11.0249 0.77861 12.6287 0.0520888 14.3262 0C17.3273 0 19.7603 2.39588 19.7603 5.35135C19.7603 8.85038 15.8104 12.6852 13.4415 14.9851C13.2282 15.1921 13.0278 15.3868 12.8442 15.5676L10.5915 17.7859C10.4527 17.9228 10.2643 17.9998 10.0678 18H9.6924C9.49594 17.9998 9.30759 17.9228 9.16876 17.7859L6.91609 15.5676C6.73248 15.3868 6.53202 15.1921 6.31874 14.9851C3.94983 12.6852 0 8.85038 0 5.35135C0 2.39588 2.43292 0 5.43407 0C7.13157 0.0520888 8.73538 0.77861 9.88013 2.01405ZM9.88027 15.7328L12.0539 13.6215C14.0596 11.6756 17.7844 8.03664 17.7844 5.35123C17.7844 4.44299 17.416 3.57235 16.7611 2.93287C16.1061 2.29339 15.2189 1.93805 14.2967 1.94583C12.7606 2.08626 11.3741 2.91055 10.5324 4.18367C10.4381 4.30499 10.2923 4.37677 10.1372 4.37826H9.69255C9.46171 4.37765 9.24364 4.27385 9.09974 4.0961C8.27139 2.8699 6.92358 2.07925 5.43421 1.94583C3.52439 1.94583 1.97617 3.47048 1.97617 5.35123C1.97617 8.03664 5.70098 11.6756 7.70664 13.6215L9.88027 15.7328Z" fill="#707070"/>
-                                </svg>
-                            </button>
-                        </div>
-                        <div className="flex ml-2">
-                            <img className="h-4 w-5 ml-2" 
-                                src={Mail} 
-                                alt="Mail" />                            
-                            <img className="h-4 w-5 ml-2" 
-                                src={Facebook} 
-                                alt="Facebook" />
-                            <img className="h-4 w-5 ml-2" 
-                                src={Instagram} 
-                                alt="Instagram" />                                
-                        </div>
-                    </div>
-                    <div className="flex flex-row mt-4">
-                        <div className="flex flex-col mr-6">
-                            <p className="font-semibold text-primary text-sm">Min Order:</p>
-                            <p className="font-semibold text-primary text-sm">Categories:</p>
-                        </div>
-                        <div className="flex flex-col">
-                            <p className="font-semibold text-sm text-gray-600">{minOrder}</p>
-                            <p className="font-semibold text-sm text-gray-600">Fruits</p>
-                        </div>
-                    </div>
+                    
                 </div>
-                
-            </div>
             </section>
             <div className="flex flex-row md:ml-24">
                 {infoItems.map((item, index) => (
