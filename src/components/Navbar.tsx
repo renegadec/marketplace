05ce85d24f8a46c3e610ea30dd57f03737ac2b9a
Isheanesu <inmisimail@gmail.com>
2023-01-30 15:43:58 +0200

    Updated link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -138,7 +138,7 @@ const Navbar = () => {
                                 </div>
                                 <div className="py-6">
                                     <a
-                                        href="#"
+                                        href="/login"
                                         onClick={() => setMobileMenuOpen(false)}
                                         className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10 cursor-pointer"
                                     >
