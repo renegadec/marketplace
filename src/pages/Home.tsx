0e10f6768f0a49e277604e6b672f0bddd3b32bb1
Isheanesu <inmisimail@gmail.com>
2023-01-30 18:23:39 +0200

    Home page scroll to top

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
-import React from "react"
+import { useEffect } from "react"
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import About from "../components/About";
 import Subscribe from "../components/Subscribe";
 
 const Home = () => {
+
+    useEffect(() => {
+        const element = document.getElementById(`main`);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [])
+
     return (
-        <div>
+        <div id="main">
             <Hero />
             <Features />
             <About />
