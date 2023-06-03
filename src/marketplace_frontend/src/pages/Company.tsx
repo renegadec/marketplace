c43035d973c43b99670f324e23c9047eeb1af535
africoiner <confidencenyirenda7@gmail.com>
2023-06-03 14:28:43 +0200

    added partners component

--- src/marketplace_frontend/src/pages/Company.tsx
+++ src/marketplace_frontend/src/pages/Company.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Team from '../components/Team';
 import Contact from '../components/Contact';
+import Partners from '../components/Partners';
 
 const Company = () => {
     return (
         <>
             <Team />
             <Contact />
+            <Partners />
         </>
         
     )
