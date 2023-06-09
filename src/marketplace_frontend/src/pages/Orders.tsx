a40e25897078e24fb3517cedf389e4e1266c94c6
africoiner <confidencenyirenda7@gmail.com>
2023-06-09 21:57:34 +0200

    added orders components

---
+++ src/marketplace_frontend/src/pages/Orders.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import OrderDetails from '../components/Orders/OrderDetails'
+import OrderHistory from '../components/Orders/OrderHistory'
+
+const Orders = () => {
+  return (
+    <div>
+      <OrderDetails />
+      <OrderHistory />
+    </div>
+  )
+}
+
+export default Orders
