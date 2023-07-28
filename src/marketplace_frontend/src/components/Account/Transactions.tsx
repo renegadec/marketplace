9c16c2e9e9c40c03e09592c888e4490deecccb5a
HarmonyMurombo <harmonysvinurai1@gmail.com>
2023-07-28 15:05:19 +0200

    updated the transactions interface

--- src/marketplace_frontend/src/components/Account/Transactions.tsx
+++ src/marketplace_frontend/src/components/Account/Transactions.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
 const Transactions = () => {
   const [transactions, setTransactions] = useState([]);
   const [filteredTransactions, setFilteredTransactions] = useState([]);
@@ -14,44 +18,7 @@ const Transactions = () => {
   useEffect(() => {
     // Fetch transactions from your API and set them to state
     // For now, let's use static data
-    const staticTransactions = [
-      {
-        id: 1,
-        date: '2023-07-18',
-        amount: '$5100',
-        status: 'completed',
-        paymentMethod: 'Credit Card',
-        shippingDetails: '7341, Rujeko B, Dendera Crescent, Masvingo',
-        trackingNumber: '123tec',
-        invoice: 'https://example.com/invoice.pdf',
-        notes: "Great news! Your order has been successfully delivered. We hope you enjoy your purchase. If you have any questions or concerns, please don't hesitate to reach out to us.",
-      },
-      {
-        id: 2,
-        date: '2023-07-19',
-        amount: '$2200',
-        status: 'pending',
-        paymentMethod: 'PayPal',
-        shippingDetails: '456 Elm St, Los Angeles, CA',
-        trackingNumber: '894bpc',
-        invoice: 'https://example.com/invoice.pdf',
-        notes: "Dear customer, thank you for your order. We are currently processing your request and will notify you once it is ready for shipment."
-      },
-      {
-        id: 3,
-        date: '2023-07-2',
-        amount: '$200',
-        status: 'pending',
-        paymentMethod: 'Ecocash',
-        shippingDetails: 'No 3, Downham Avenue , Harare',
-        trackingNumber: '7341abc',
-        invoice: 'https://example.com/invoice.pdf',
-        notes: "We have a lot of orders be patient we are currently processing your request"
-      },
-      
-      
-
-    ];
+    const staticTransactions = [];
 
     setTransactions(staticTransactions);
     setFilteredTransactions(staticTransactions);
@@ -71,32 +38,75 @@ const Transactions = () => {
 
   return (
     <div>
-     <h1 style={{ fontWeight: 'bold' }}>Transactions</h1>
+      <h1 style={{fontWeight: 'bold'}}>Transactions</h1>
       <div>
         {/* Filter and date range input fields */}
       </div>
       <div>
         {filteredTransactions.map((transaction) => (
-          <div key={transaction.id} style={{ backgroundColor: 'white', padding: '10px', marginBottom: '10px' }}>
+          <div key={transaction.id} style={{ backgroundColor: 'white', padding: '10px', marginBottom: '10px'}}>
             <p>Date: {transaction.date}</p>
             <p>Amount: {transaction.amount}</p>
             <p>Status: {transaction.status}</p>
             <p>Payment Method: {transaction.paymentMethod}</p>
             <p>Shipping Details: {transaction.shippingDetails}</p>
             <p>Tracking Number: {transaction.trackingNumber}</p>
-            <button onClick={() => handleDownloadInvoice(transaction.invoice)}className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Download Invoice</button>
             <div>
-              {transaction.notes && <p>Notes: {transaction.notes}</p>}</div>
+              {transaction.notes && <p>Notes: {transaction.notes}</p>}
+            </div>
           </div>
         ))}
       </div>
-      <div>
-        <h3>Leave Feedback</h3>
-        <textarea className="form-control"></textarea>
-        <button type="submit"className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onClick={handleSubmitFeedback}>Submit</button>
-      </div>
-    </div>
+      <div className="collapse bg-base-200">
+  <input type="radio" name="my-accordion-1" checked={true} /> 
+  <div className="collapse-title text-l font-bold">
+    Click to open your transaction
+  </div>
+  <div className="collapse-content font-semibold"> 
+    <p>Date:10/05/2023</p>
+    <p>Amount:$1500</p>
+    <p>Status:Completed</p>
+    <p>Payment Method:Paypal</p>
+    <p>Shipping Details: 7341,Rujeko B,Masvingo</p>
+    <p>Tracking Number:abc788</p>
+    <p>Notes:Your order has been delivered successfully,Thank you for your patience.</p>
+    <button onClick={() => handleDownloadInvoice('invoiceUrl')} className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Download Invoice</button>
+  </div>
+</div>
+<div className="collapse bg-base-200">
+  <input type="radio" name="my-accordion-1" /> 
+  <div className="collapse-title text-l font-bold">
+    Click to open your transaction
+  </div>
+  <div className="collapse-content font-semibold"> 
+    <p>Date:4/08/2022</p>
+    <p>Amount:$2800</p>
+    <p>Status:Pending</p>
+    <p>Payment Method:GooglePay</p>
+    <p>Shipping Details: No 3,Downham Avenue,Harare</p>
+    <p>Tracking Number:xya127</p>
+    <p>Notes:We have a lot of orders,please your order is under processing.</p>
+    <button onClick={() => handleDownloadInvoice('invoiceUrl')} className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Download Invoice</button> 
+  </div>
+</div>
+<div className="collapse bg-base-200">
+  <input type="radio" name="my-accordion-1" /> 
+  <div className="collapse-title text-l font-bold">
+    Click to open your transaction
+  </div>
+  <div className="collapse-content font-semibold"> 
+    <p>Date:8/01/2021</p>
+    <p>Amount:$4800</p>
+    <p>Status:Completed</p>
+    <p>Payment Method:Paypal</p>
+    <p>Shipping Details: No 8,Drayton Avenue,Harare</p>
+    <p>Tracking Number:xya127</p>
+    <p>Notes:Congratulations,your order is successfully deliverd.</p>
+    <button onClick={() => handleDownloadInvoice('invoiceUrl')} className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Download Invoice</button> 
+  </div>
+</div>
+    </div> 
+
   );
 };
-
 export default Transactions;
