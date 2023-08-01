fea1ef4d59c6cb7d8901ff2a89b7919959f58c65
africoiner <confidencenyirenda7@gmail.com>
2023-08-01 15:10:41 +0200

    replaced transactions component

--- src/marketplace_frontend/src/components/Account/Transactions.tsx
+++ src/marketplace_frontend/src/components/Account/Transactions.tsx
@@ -1,110 +1,124 @@
-import React, { useState, useEffect } from 'react';
+const orders = [
+  {
+    number: 'WU88191111',
+    date: 'January 22, 2021',
+    datetime: '2021-01-22',
+    invoiceHref: '#',
+    total: '$238.00',
+    products: [
+      {
+        id: 1,
+        name: 'Machined Pen and Pencil Set',
+        href: '#',
+        price: '$70.00',
+        status: 'Delivered Jan 25, 2021',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/order-history-page-02-product-01.jpg',
+        imageAlt: 'Detail of mechanical pencil tip with machined black steel shaft and chrome lead tip.',
+      },
+      // More products...
+    ],
+  },
+  // More orders...
+]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
-const Transactions = () => {
-  const [transactions, setTransactions] = useState([]);
-  const [filteredTransactions, setFilteredTransactions] = useState([]);
-  const [paymentMethods, setPaymentMethods] = useState([
-    { name: 'Credit Card', icon: 'credit-card' },
-    { name: 'Ecocash', icon: 'eco-cash'},
-    { name: 'PayPal', icon: 'paypal' },
-    { name: 'Apple Pay', icon: 'apple-pay' },
-    { name: 'Google Pay', icon: 'google-pay' },
-  ]);
-
-  useEffect(() => {
-    // Fetch transactions from your API and set them to state
-    // For now, let's use static data
-    const staticTransactions = [];
-
-    setTransactions(staticTransactions);
-    setFilteredTransactions(staticTransactions);
-  }, []);
-
-  const handleFilterClick = () => {
-    // Filter transactions based on date range
-  };
-
-  const handleDownloadInvoice = (invoiceUrl) => {
-    // Handle download invoice
-  };
-
-  const handleSubmitFeedback = (e) => {
-    // Handle submit feedback
-  };
-
+export default function Transactions() {
   return (
-    <div>
-      <h1 style={{fontWeight: 'bold'}}>Transactions</h1>
-      <div>
-        {/* Filter and date range input fields */}
-      </div>
-      <div>
-        {filteredTransactions.map((transaction) => (
-          <div key={transaction.id} style={{ backgroundColor: 'white', padding: '10px', marginBottom: '10px'}}>
-            <p>Date: {transaction.date}</p>
-            <p>Amount: {transaction.amount}</p>
-            <p>Status: {transaction.status}</p>
-            <p>Payment Method: {transaction.paymentMethod}</p>
-            <p>Shipping Details: {transaction.shippingDetails}</p>
-            <p>Tracking Number: {transaction.trackingNumber}</p>
-            <div>
-              {transaction.notes && <p>Notes: {transaction.notes}</p>}
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="collapse bg-base-200">
-  <input type="radio" name="my-accordion-1" checked={true} /> 
-  <div className="collapse-title text-l font-bold">
-  <p>Date:10/05/2023</p>
-  </div>
-  <div className="collapse-content font-semibold"> 
-   
-    <p>Amount:$1500</p>
-    <p>Status:Completed</p>
-    <p>Payment Method:Paypal</p>
-    <p>Shipping Details: 7341,Rujeko B,Masvingo</p>
-    <p>Tracking Number:abc788</p>
-    <p>Notes:Your order has been delivered successfully,Thank you for your patience.</p>
-    <button onClick={() => handleDownloadInvoice('invoiceUrl')} className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Download Invoice</button>
-  </div>
-</div>
-<div className="collapse bg-base-200">
-  <input type="radio" name="my-accordion-1" /> 
-  <div className="collapse-title text-l font-bold">
-  <p>Date:4/08/2022</p>
-  </div>
-  <div className="collapse-content font-semibold"> 
-    <p>Amount:$2800</p>
-    <p>Status:Pending</p>
-    <p>Payment Method:GooglePay</p>
-    <p>Shipping Details: No 3,Downham Avenue,Harare</p>
-    <p>Tracking Number:xya127</p>
-    <p>Notes:We have a lot of orders,please your order is under processing.</p>
-    <button onClick={() => handleDownloadInvoice('invoiceUrl')} className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Download Invoice</button> 
-  </div>
-</div>
-<div className="collapse bg-base-200">
-  <input type="radio" name="my-accordion-1" /> 
-  <div className="collapse-title text-l font-bold">
-  <p>Date:8/01/2021</p>
-  </div>
-  <div className="collapse-content font-semibold"> 
-    <p>Amount:$4800</p>
-    <p>Status:Completed</p>
-    <p>Payment Method:Paypal</p>
-    <p>Shipping Details: No 8,Drayton Avenue,Harare</p>
-    <p>Tracking Number:xya127</p>
-    <p>Notes:Congratulations,your order is successfully deliverd.</p>
-    <button onClick={() => handleDownloadInvoice('invoiceUrl')} className="inline-flex items-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Download Invoice</button> 
-  </div>
-</div>
-    </div> 
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8 lg:pb-24">
+        <div className="max-w-xl">
+          <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Transactions history</h1>
+          <p className="mt-2 text-sm text-gray-500">
+            Check the status of recent orders, manage returns, and download invoices.
+          </p>
+        </div>
 
-  );
-};
-export default Transactions;
+        <div className="mt-16">
+          <h2 className="sr-only">Recent orders</h2>
+
+          <div className="space-y-20">
+            {orders.map((order) => (
+              <div key={order.number}>
+                <h3 className="sr-only">
+                  Order placed on <time dateTime={order.datetime}>{order.date}</time>
+                </h3>
+
+                <div className="rounded-lg bg-gray-50 py-6 px-4 sm:flex sm:items-center sm:justify-between sm:space-x-6 sm:px-6 lg:space-x-8">
+                  <dl className="flex-auto space-y-6 divide-y divide-gray-200 text-sm text-gray-600 sm:grid sm:grid-cols-3 sm:gap-x-6 sm:space-y-0 sm:divide-y-0 lg:w-1/2 lg:flex-none lg:gap-x-8">
+                    <div className="flex justify-between sm:block">
+                      <dt className="font-medium text-gray-900">Date placed</dt>
+                      <dd className="sm:mt-1">
+                        <time dateTime={order.datetime}>{order.date}</time>
+                      </dd>
+                    </div>
+                    <div className="flex justify-between pt-6 sm:block sm:pt-0">
+                      <dt className="font-medium text-gray-900">Order number</dt>
+                      <dd className="sm:mt-1">{order.number}</dd>
+                    </div>
+                    <div className="flex justify-between pt-6 font-medium text-gray-900 sm:block sm:pt-0">
+                      <dt>Total amount</dt>
+                      <dd className="sm:mt-1">{order.total}</dd>
+                    </div>
+                  </dl>
+                  <a
+                    href={order.invoiceHref}
+                    className="mt-6 flex w-full items-center justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto"
+                  >
+                    View Invoice
+                    <span className="sr-only">for order {order.number}</span>
+                  </a>
+                </div>
+
+                <table className="mt-4 w-full text-gray-500 sm:mt-6">
+                  <caption className="sr-only">Products</caption>
+                  <thead className="sr-only text-left text-sm text-gray-500 sm:not-sr-only">
+                    <tr>
+                      <th scope="col" className="py-3 pr-8 font-normal sm:w-2/5 lg:w-1/3">
+                        Product
+                      </th>
+                      <th scope="col" className="hidden w-1/5 py-3 pr-8 font-normal sm:table-cell">
+                        Price
+                      </th>
+                      <th scope="col" className="hidden py-3 pr-8 font-normal sm:table-cell">
+                        Status
+                      </th>
+                      <th scope="col" className="w-0 py-3 text-right font-normal">
+                        Info
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 border-b border-gray-200 text-sm sm:border-t">
+                    {order.products.map((product) => (
+                      <tr key={product.id}>
+                        <td className="py-6 pr-8">
+                          <div className="flex items-center">
+                            <img
+                              src={product.imageSrc}
+                              alt={product.imageAlt}
+                              className="mr-6 h-16 w-16 rounded object-cover object-center"
+                            />
+                            <div>
+                              <div className="font-medium text-gray-900">{product.name}</div>
+                              <div className="mt-1 sm:hidden">{product.price}</div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="hidden py-6 pr-8 sm:table-cell">{product.price}</td>
+                        <td className="hidden py-6 pr-8 sm:table-cell">{product.status}</td>
+                        <td className="whitespace-nowrap py-6 text-right font-medium">
+                          <a href={product.href} className="text-indigo-600">
+                            View<span className="hidden lg:inline">Product</span>
+                            <span className="sr-only">, {product.name}</span>
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
